7c98343d5bbbb4f1ed46248ba882fd9d4f84ca14
AKASH <45868390+Akash97p@users.noreply.github.com>
2020-03-05 16:44:29 +0530

    Update serial_test.py

--- pyserial/serial_test.py
+++ pyserial/serial_test.py
@@ -1,19 +1,20 @@
 import serial
 
-ser = serial.Serial('/dev/serial0',38400)
-def send_data():
+ser = serial.Serial('/dev/serial0',38400) #GPIO14 = Tx , GPIO15 = Rx
+
+def send_json_object():
 	c1 = 9
 	c2 = 2
 	c3 = 7
 	c4 = 8
-	string = '{"c1":'+str(c1)+',"c2":'+str(c2)+',"c3":'+str(c3)+',"c4":'+str(c4)+'}#'
-	ser.write(string.encode())
-	#if ser.is_open():
-	#	ser.write(string.encode())
+	json_object = '{"c1":'+str(c1)+',"c2":'+str(c2)+',"c3":'+str(c3)+',"c4":'+str(c4)+'}#' 
+	ser.write(json_object.encode())
+	#if (ser.is_open() == True):
+	#	ser.write(json_object.encode())
 		#ser.write(b'hii')
 	#else: 
 	#	ser.open()
-	#	ser.write(string.encode())
+	#	ser.write(json_object.encode())
 	#ser.close()
 
-send_data()
+send_json_object()
